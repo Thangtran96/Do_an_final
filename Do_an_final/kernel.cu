5a945913c406d8ab9b29f5edd24f29b72ced917c
DESKTOP-MT\ThangTran <minhthang.tran.398.15@gmail.com>
2018-04-24 23:07:07 +0700

    dm vua chay dc bay h lai ko :(

--- Do_an_final/kernel.cu
+++ Do_an_final/kernel.cu
@@ -8,20 +8,31 @@
 #include <string>
 #include <string.h>
 #include <utility>
+#include <ctime>
+#include <time.h>
 
 using namespace std;
 // output
 ofstream fo("Ans.txt");
 
 // Cac bien hang so
-const int ARRAY_SIZE = 12005;
-const int ARRAY_BYTES_INT = ARRAY_SIZE * sizeof(int);
+const int ARRAY_SIZE_INP = 12005;
+const int ARRAY_BYTES_INP = ARRAY_SIZE_INP * sizeof(int);
+const int ARRAY_SIZE_OUT = 605;
+const int ARRAY_BYTES_OUT = ARRAY_SIZE_OUT * sizeof(int);
 
 //cac bien chinh
 int l = 9, d = 3;
-char cDataInp[ARRAY_SIZE];
-int h_dataMotif[12005];
+char cDataInp[ARRAY_SIZE_INP];
+int h_dataMotif[ARRAY_SIZE_INP];
 string sDataInp[20];
+
+struct Motif_Ans
+{
+	int dis;
+	string motif;
+	int adress[20];
+};
 
 //input tu file
 void File_Input()
@@ -33,7 +44,7 @@ void File_Input()
 		perror("Error opening file");
 	else
 	{
-		if (fgets(cDataInp, ARRAY_SIZE, pFile) != NULL)
+		if (fgets(cDataInp, ARRAY_SIZE_INP, pFile) != NULL)
 			cout << "nhap du lieu thanh cong!\n";
 		fclose(pFile);
 	}
@@ -52,12 +63,45 @@ void File_Input()
 	}
 	int k = 0;
 	string temp = cDataInp;
-	cout << temp << endl;
+	//cout << temp << endl;
 	for (int i = 0; i < temp.size(); i += 600) {
 		sDataInp[k] = temp.substr(i, 600);
-		cout << k << ". " << sDataInp[k] << endl;
+		//cout << k << ". " << sDataInp[k] << endl;
 		k++;
 	}
+}
+
+int score_ham(string s1, string s2)
+{
+	int res = 0;
+	for (int i = 0; i<s1.size(); ++i) if (s1[i] != s2[i]) res++;
+	return res;
+}
+
+Motif_Ans dis_hamming(string s)
+{
+	Motif_Ans res;
+	res.motif = s;
+	int res_Sum = 0, temp_score = 999, temp_Adress;
+	for (int i = 0; i<20; ++i)
+	{
+		string s1 = sDataInp[i];
+		temp_score = 999;
+		for (int j = 0; j < s1.size() - l + 1; ++j)
+		{
+			string temp_str = s1.substr(j, l);
+			int score_s = score_ham(s, temp_str);
+			if (score_s < temp_score)
+			{
+				temp_score = score_s;
+				temp_Adress = j + 1;
+			}
+		}
+		res_Sum += temp_score;
+		res.adress[i] = temp_Adress;
+	}
+	res.dis = res_Sum;
+	return res;
 }
 
 __global__ void patternBarching(const int* d_datainp, const int l, const int d, int *ans) {
@@ -234,12 +278,123 @@ __global__ void patternBarching(const int* d_datainp, const int l, const int d, 
 			}
 		}//end Ne
 		//end tinh
+
+		int dem = 0;
+		int res = 0;
+		for (int i = 0; i < l; ++i) {
+			res = res | (ansMotif_string[i] << dem);
+			dem += 2;
+		}
+		ans[index] = res;
 	}
 }
 
+
 int main()
 {
-	printf("ggg\n");
-	//File_Input();
+	File_Input();
+	int h_dataOut[ARRAY_SIZE_OUT];
+	for (int i = 0; i < 600; ++i) {
+		h_dataOut[i] = -1;
+	}
+	//GPU khoi tao bien va bo nho
+	int *d_dataMotif;
+	if (cudaMalloc(&d_dataMotif, ARRAY_BYTES_INP) != cudaSuccess) {
+		cout << "error allocating memory!" << endl;
+		return 0;
+	}
+	int *d_dataOut;
+	if (cudaMalloc(&d_dataOut, ARRAY_BYTES_OUT) != cudaSuccess) {
+		cout << "error allocating memory!" << endl;
+		cudaFree(d_dataMotif);
+		return 0;
+	}
+	if (cudaMemcpy(d_dataMotif, h_dataMotif, ARRAY_BYTES_INP, cudaMemcpyHostToDevice) != cudaSuccess) {
+		cout << "error copying memory!" << endl;
+		cudaFree(d_dataMotif);
+		cudaFree(d_dataOut);
+		return 0;
+	}
+	if (cudaMemcpy(d_dataOut, h_dataOut, ARRAY_BYTES_OUT, cudaMemcpyHostToDevice) != cudaSuccess) {
+		cout << "error copying memory!" << endl;
+		cudaFree(d_dataMotif);
+		cudaFree(d_dataOut);
+		return 0;
+	}
+	//clock_t t = clock();
+	cout << "dang chay ...." << endl;
+
+	//khoi tao chay cuda
+	int threadsPerBlock = 256;
+	int blocksPerGrid = (600 + threadsPerBlock - 1) / threadsPerBlock;
+	patternBarching << <blocksPerGrid, threadsPerBlock >> > (d_dataMotif, l, d, d_dataOut);
+	//t = clock() - t;
+	//fo << "Thoi gian chay: " << clock()-t/ CLOCKS_PER_SEC << endl;
+	//cout << "\nTime " << clock() / (double)1000 << " Sec";
+
+	//copy data tro ve
+	if (cudaMemcpy(h_dataOut, d_dataOut, ARRAY_BYTES_OUT, cudaMemcpyDeviceToHost) != cudaSuccess) {
+		cout << "error copying memory!" << endl;
+		cudaFree(d_dataMotif);
+		cudaFree(d_dataOut);
+		return 0;
+	}
+	//lay best motif
+	cout << "\n du lieu tra ve" << endl;
+	Motif_Ans best_motif,temp_motif_return;
+	best_motif.dis = 999;
+	for (int i = 0; i < 600; i++)
+	{
+		int chuyenStr = d_dataOut[i];
+		int k = 0;
+		string res = "";
+		if (chuyenStr > 0) {
+			//chuyen kieu in sang string
+			for (int j = 0; j < l; ++j) {
+				int temp = (chuyenStr >> k) & 3;
+				//cout << temp << ' ';
+				switch (temp)
+				{
+				case 0:
+				{
+					res += 'A'; break;
+				}
+				case 1:
+				{
+					res += 'C'; break;
+				}
+				case 2:
+				{
+					res += 'G'; break;
+				}
+				case 3:
+				{
+					res += 'T'; break;
+				}
+				}
+				k += 2;
+			}
+			//ket thuc chuyen
+			//kiem tra do dai va tra vi tri
+			cout << res << endl;
+			//temp_motif_return = dis_hamming(res);
+			//if (temp_motif_return.dis < best_motif.dis) {
+			//	best_motif.dis = temp_motif_return.dis;
+			//	best_motif.motif = temp_motif_return.motif;
+			//	for (int z = 0; z < 20; ++z) {
+			//		best_motif.adress[z] = temp_motif_return.adress[z];
+			//	}
+			//}
+			//end kiem tra
+		}
+	}
+	/*fo << "Best motif: " << best_motif.motif << endl << "Motif location: " << endl;
+	for (int z = 0; z < 20; ++z) {
+		fo << best_motif.adress[z] << ' ';
+	}*/
+	fo << "\nTime " << clock() / (double)1000 << " Sec";
+
+	cudaFree(d_dataMotif);
+	cudaFree(d_dataOut);
 	return 0;
 }
